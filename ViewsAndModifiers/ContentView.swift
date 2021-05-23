ca08c48bb0073c962d0b297fa18043baef50387a
Waihon Yew <yewwaihon@gmail.com>
2021-05-23 22:59:35 +0800

    Extract smaller view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,22 +7,24 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+        .font(.largeTitle)
+        .padding()
+        .foregroundColor(.white)
+        .background(Color.blue)
+        .clipShape(Capsule())
+  }
+}
+
 struct ContentView: View {
   var body: some View {
     VStack {
-      Text("First")
-          .font(.largeTitle)
-          .padding()
-          .foregroundColor(.white)
-          .background(Color.blue)
-          .clipShape(Capsule())
-
-      Text("Second")
-          .font(.largeTitle)
-          .padding()
-          .foregroundColor(.white)
-          .background(Color.blue)
-          .clipShape(Capsule())
+      CapsuleText(text: "First")
+      CapsuleText(text: "Second")
     }
   }
 }
