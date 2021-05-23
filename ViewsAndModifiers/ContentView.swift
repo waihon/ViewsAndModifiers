9d6838b7eaf8889aaba42cd0d32a8d3520f34cb8
Waihon Yew <yewwaihon@gmail.com>
2021-05-24 00:16:09 +0800

    Create a new view structure by embedding the existing one in a ZStack

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,27 +7,32 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
+struct Watermark: ViewModifier {
+  var text: String
+  
   func body(content: Content) -> some View {
-    content
-      .font(.largeTitle)
-      .foregroundColor(.white)
-      .padding()
-      .background(Color.blue)
-      .clipShape(RoundedRectangle(cornerRadius: 10))
+    ZStack(alignment: .bottomTrailing) {
+      content
+      Text(text)
+        .font(.caption)
+        .foregroundColor(.white)
+        .padding(5)
+        .background(Color.black)
+    }
   }
 }
 
 extension View {
-  func titleStyle() -> some View {
-    self.modifier(Title())
+  func watermarked(with text: String) -> some View {
+    self.modifier(Watermark(text: text))
   }
 }
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello World")
-      .titleStyle()
+    Color.blue
+      .frame(width: 300, height: 300)
+      .watermarked(with: "100 Days of SwiftUI")
   }
 }
 
