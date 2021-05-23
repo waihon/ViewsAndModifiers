09ca94357750af67c35d3da26b49ef0570d22917
Waihon Yew <yewwaihon@gmail.com>
2021-05-23 23:59:45 +0800

    Create a view modifier called Title for Text view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,26 +7,21 @@
 
 import SwiftUI
 
-struct CapsuleText: View {
-  var text: String
-  
-  var body: some View {
-    Text(text)
-        .font(.largeTitle)
-        .padding()
-        .background(Color.blue)
-        .clipShape(Capsule())
+struct Title: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.largeTitle)
+      .foregroundColor(.white)
+      .padding()
+      .background(Color.blue)
+      .clipShape(RoundedRectangle(cornerRadius: 10))
   }
 }
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      CapsuleText(text: "First")
-        .foregroundColor(.white)
-      CapsuleText(text: "Second")
-        .foregroundColor(.yellow)
-    }
+    Text("Hello World")
+      .modifier(Title())
   }
 }
 
