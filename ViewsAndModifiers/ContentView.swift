1484788a98aafb9fb5bdff0f873507bb25306308
Waihon Yew <yewwaihon@gmail.com>
2021-05-23 10:39:40 +0800

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Yew Wai Hon on 23/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
