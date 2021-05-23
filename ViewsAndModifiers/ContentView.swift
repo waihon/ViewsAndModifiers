839842f3b4964a0e9e3315559cdb479ea4bdc5e9
Waihon Yew <yewwaihon@gmail.com>
2021-05-24 01:13:15 +0800

    Create and use a custom container

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,32 +7,29 @@
 
 import SwiftUI
 
-struct Watermark: ViewModifier {
-  var text: String
+struct GridStack<Content: View>: View {
+  let rows: Int
+  let columns: Int
+  let content: (Int, Int) -> Content
   
-  func body(content: Content) -> some View {
-    ZStack(alignment: .bottomTrailing) {
-      content
-      Text(text)
-        .font(.caption)
-        .foregroundColor(.white)
-        .padding(5)
-        .background(Color.black)
+  var body: some View {
+    VStack {
+      ForEach(0..<rows, id: \.self) { row in
+        HStack {
+          ForEach(0..<self.columns, id: \.self) { column in
+            self.content(row, column)
+          }
+        }
+      }
     }
-  }
-}
-
-extension View {
-  func watermarked(with text: String) -> some View {
-    self.modifier(Watermark(text: text))
   }
 }
 
 struct ContentView: View {
   var body: some View {
-    Color.blue
-      .frame(width: 300, height: 300)
-      .watermarked(with: "100 Days of SwiftUI")
+    GridStack(rows: 4, columns: 4) { row, col in
+      Text("R\(row) C\(col)")
+    }
   }
 }
 
