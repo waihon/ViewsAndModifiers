8ff91fad023d8937af79fb0b9741c2d12428e6b6
Waihon Yew <yewwaihon@gmail.com>
2021-05-25 00:02:32 +0800

    Create a modifier that makes a view have a large, blue font

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,36 +7,18 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-  let rows: Int
-  let columns: Int
-  let content: (Int, Int) -> Content
-  
-  init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-    self.rows = rows
-    self.columns = columns
-    self.content = content
-  }
-  
-  var body: some View {
-    VStack {
-      ForEach(0..<rows, id: \.self) { row in
-        HStack {
-          ForEach(0..<self.columns, id: \.self) { column in
-            self.content(row, column)
-          }
-        }
-      }
-    }
+struct Title: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.largeTitle)
+      .foregroundColor(.blue)
   }
 }
 
 struct ContentView: View {
   var body: some View {
-    GridStack(rows: 4, columns: 4) { row, col in
-      Image(systemName: "\(row * 4 + col).circle")
-      Text("R\(row) C\(col)")
-    }
+    Text("Hello World!")
+      .modifier(Title())
   }
 }
 
