22cd4d28bd1e54ab9d398134c5288ee674f09c76
Waihon Yew <yewwaihon@gmail.com>
2021-05-24 23:47:56 +0800

    Leverage on view builder to form an implicit stack for us

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -12,6 +12,12 @@ struct GridStack<Content: View>: View {
   let columns: Int
   let content: (Int, Int) -> Content
   
+  init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+    self.rows = rows
+    self.columns = columns
+    self.content = content
+  }
+  
   var body: some View {
     VStack {
       ForEach(0..<rows, id: \.self) { row in
@@ -28,10 +34,8 @@ struct GridStack<Content: View>: View {
 struct ContentView: View {
   var body: some View {
     GridStack(rows: 4, columns: 4) { row, col in
-      HStack {
-        Image(systemName: "\(row * 4 + col).circle")
-        Text("R\(row) C\(col)")
-      }
+      Image(systemName: "\(row * 4 + col).circle")
+      Text("R\(row) C\(col)")
     }
   }
 }
